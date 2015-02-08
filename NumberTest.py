707500f5692d8a32f90c26cd569206c1d0cf827f
Michael Chary <mac389@gmail.com>
2015-02-08 15:04:59 -0500

    update

--- NumberTest.py
+++ NumberTest.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk import load_parser
-cp = load_parser('NumbersTest.fcfg')
+cp = load_parser('NumberTest.fcfg')
 tokens = "Cocaine is good".split()
 for tree in cp.parse(tokens):
     print (tree)
@@ -9,6 +9,6 @@ for tree in cp.parse(tokens2):
     print (tree)
 tokens3 = "Cocaines is good".split()
 for tree in cp.parse(tokens3):
-    print (tree)
     if tree == "":
         print ("Error")
+    tree.draw()
