6e2ad9c444bf36dad8c5fb415f5363937fe5fb25
DavidYi1 <yid@bxscience.edu>
2015-02-14 21:04:55 -0500

    Updated for second comment
    Awkward parsing for part4. Need to understand why tree acts as such.

--- CommentParsing.py
+++ CommentParsing.py
@@ -1,14 +1,27 @@
 import nltk 
 
+
+
 from nltk.draw.util import CanvasFrame
 from nltk.draw import TreeWidget
 from nltk import Tree, word_tokenize,load_parser
 
+'''
+   1. Run pyStatParser first to get an approximation.
+   2. From this approximation add to grammar.cfg
+'''
+#Grammar induction 
+#sentence = "Smoking cocaine and pot is thrilling"
+#sentence = 'Smoking cocaine and pot is thrilling'
+sentence = "smoking_pot and cocaine is thrilling"
+filename = 'test'
 part1 = 'cocaine is pretty_sh$% in my_opinion'
-part2 = 'get some_MMDA'
+part2 = 'you get some MMDA'
+part3 = 'MDMA is a blank_canvas' 
+part4 = 'you can do anything' 
+
 parser = load_parser('grammar.fcfg')
 
-for tree in parser.parse(part1.split()):
+for tree in parser.parse(part4.split()):
     tree.draw()
     print (tree)
-
