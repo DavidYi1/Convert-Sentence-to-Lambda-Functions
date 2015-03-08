a848ba5c1091b32c4e15992bbb6b2900619c7aaa
Michael Chary <mac389@gmail.com>
2015-03-08 15:06:29 -0400

    update

--- CommentParsing.py
+++ CommentParsing.py
@@ -1,7 +1,5 @@
 import nltk 
 
-
-
 from nltk.draw.util import CanvasFrame
 from nltk.draw import TreeWidget
 from nltk import Tree, word_tokenize,load_parser
@@ -16,12 +14,12 @@ from nltk import Tree, word_tokenize,load_parser
 sentence = "smoking_pot and cocaine is thrilling"
 filename = 'test'
 part1 = 'cocaine is pretty_sh$% in my_opinion'
-part2 = 'you get some MMDA'
+part2 = 'you get some MDMA' #'you get some MDMA'
 part3 = 'MDMA is a blank_canvas' 
 part4 = 'you can do anything_you_want_with_it' 
 
 parser = load_parser('grammar.fcfg')
 
-for tree in parser.parse(part4.split()):
+for tree in parser.parse(part2.split()):
     tree.draw()
     print (tree)
--- to-lambda-functions.py
+++ to-lambda-functions.py
@@ -1,4 +1,4 @@
-import nltk 
+import nltk, string 
 
 from awesome_print import ap 
 from stat_parser import Parser
@@ -6,41 +6,46 @@ from stat_parser import Parser
 from nltk.draw.util import CanvasFrame
 from nltk.draw import TreeWidget
 from nltk import Tree
-from nltk import word_tokenize
+from nltk import word_tokenize, load_parser
 
 '''
 
-   1. Run pyStatParser first to get an approximation.
-   2. From thris approximation add to grammar.cfg
+	 1. Run pyStatParser first to get an approximation.
+	 2. From thris approximation add to grammar.cfg
 
 '''
 #Grammar induction 
 #sentence = "Smoking cocaine and pot is thrilling"
 #sentence = 'Smoking cocaine and pot is thrilling'
-sentence = "John saw Mary"
-filename = 'test'
+#sentence = "1 agree with 2"
+#sentence = "Moreover, dextromethorphan abuse is likely underreported to DAWN for 2 reasons."
+sentence = 'dextromethorphan abuse' 
+weasel_words = set(['moreover','likely'])
+punkt = set(string.punctuation)
+def deweasel(tokens):
+	return [token for token in tokens if token.lower() not in weasel_words
+		and not token.lower().isdigit()]
 
+filename = 'for-lovasi'
 approximation = False
 if approximation:
-  parser = Parser()
-  tree = parser.parse(sentence)
+	parser = Parser()
+	tree = parser.parse(sentence)
 
-  cf = CanvasFrame()
-  tc = TreeWidget(cf.canvas(),tree)
-  cf.add_widget(tc,20,20)
-  cf.print_to_file('%s.ps'%filename)
-  cf.destroy()
+	cf = CanvasFrame()
+	tc = TreeWidget(cf.canvas(),tree)
+	cf.add_widget(tc,20,20)
+	cf.print_to_file('%s.ps'%filename)
+	cf.destroy()
 
 else:
-  #sentence = nltk.word_tokenize(sentence)
-  sentence = ['smoking','pot','and','cocaine','is','thrilling']
-  grammar = nltk.data.load('./grammar2.cfg')
-
-  parser = nltk.ChartParser(grammar)
-  dotname = '%s.dot'%(filename)
-  for tree in parser.parse(sentence):
-    tree.draw()
-  '''
-  with open(dotname,'wb') as outfile:
-    print>>outfile,parses.dot_digraph()
-  '''
+	cp = load_parser('./bip-grammar.fcfg',trace=2)
+	for tree in cp.parse(deweasel([token for token in word_tokenize(sentence) if token not in punkt])):
+		tree.draw()
+		'''
+		semantic_value =  tree.label()['SEM']
+		for item in semantic_value.predicates():
+			print item.name
+		for item in semantic_value.constants():
+			print item.name
+		'''
