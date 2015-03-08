2ab296084e2351e85755a47f2e7d8b3c83ee9776
DavidYi1 <yid@bxscience.edu>
2015-03-08 13:49:32 -0400

    Update MetaphorandVerbnet.py
    Obtained a method to find the verb sense of the verb used in run. If metaphor then it spits out all senses of run. However, if literal, gives the usage of run in the script. However, I can't get it to actually print metaphor or literal, shown on the last line.

--- MetaphorandVerbnet.py
+++ MetaphorandVerbnet.py
@@ -1,26 +1,35 @@
+import nltk 
+from nltk.draw.util import CanvasFrame
+from nltk.draw import TreeWidget
+from nltk import Tree, word_tokenize,load_parser
+from nltk.corpus import verbnet as vn
+verbsenses = (vn.classids('run'))
 
 metaphor1 = " I run a race" 
 metaphor2 = " I run an errand" 
 parser = load_parser('grammar.fcfg')
-
 for tree in parser.parse(metaphor1.split()):
-    print (tree.label()['SEM'])
-for tree in parser.parse(metaphor2.split()):
-    print (tree.label()['SEM'])
-
-from nltk.corpus import verbnet as vn
-print (vn.classids('run'))
-v = vn.vnclass('run-51.3.2')
-print (vn.lemmas('run-51.3.2'))
-print (vn.pprint_themroles('run-51.3.2')) #Obtain the theme of verb "run"
-print([t.attrib['type'] for t in v.findall('THEMROLES/THEMROLE')])
-
+    lambdaexpression = (tree.label()['SEM'])
 for word in metaphor1.split():
-    if vn.classids(word) != []:##Very interesting part here.
-        
-        
-        print (vn.classids(word))# It returns the same as print (vn.classids('run')) 
-                                 #but also returns the actual sense used in context.
+    if vn.classids(word) != []:
+        start = set(vn.classids(word))
+        for i in start:
+            if i == verbsenses:
+                del i
+            final = list(start)
+print (lambdaexpression,final)
+    
+for tree in parser.parse(metaphor2.split()):
+    lambdaexpression = (tree.label()['SEM'])
 for word in metaphor2.split():
     if vn.classids(word) != []:
-        print (vn.classids(word))
+        start = set(vn.classids(word))
+        for i in start:
+            if i == verbsenses:
+                del i
+            final = list(start)
+print (lambdaexpression,final)
+
+if final == verbsenses:
+    sense = "metaphor"
+    print (sense) ##It should print out metaphor but doesn't.
