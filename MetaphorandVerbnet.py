e66b9d48aaf92b1d931593bad20b5e26019685dc
DavidYi1 <yid@bxscience.edu>
2015-03-15 15:01:48 -0400

    Update MetaphorandVerbnet.py

--- MetaphorandVerbnet.py
+++ MetaphorandVerbnet.py
@@ -10,26 +10,25 @@ metaphor2 = " I run an errand"
 parser = load_parser('grammar.fcfg')
 for tree in parser.parse(metaphor1.split()):
     lambdaexpression = (tree.label()['SEM'])
-for word in metaphor1.split():
-    if vn.classids(word) != []:
-        start = set(vn.classids(word))
-        for i in start:
-            if i == verbsenses:
-                del i
-            final = list(start)
+for word,pos in nltk.pos_tag(nltk.word_tokenize(metaphor1)):
+    if 'V' in pos: #Another way to focus on only verbs
+       final = [sense for sense in vn.classids(word) if 'run' in sense]
+       print (final)
+       final = str(final).strip('['']')
+       print (final)
+       unnatural = (vn.pprint_themroles('run-51.3.2'))
+       #print ((vn.pprint_themroles(final)))
+       
 print (lambdaexpression,final)
     
 for tree in parser.parse(metaphor2.split()):
     lambdaexpression = (tree.label()['SEM'])
-for word in metaphor2.split():
-    if vn.classids(word) != []:
-        start = set(vn.classids(word))
-        for i in start:
-            if i == verbsenses:
-                del i
-            final = list(start)
+for word,pos in nltk.pos_tag(nltk.word_tokenize(metaphor2)):
+    if 'V' in pos: #Another way to focus on only verbs
+       final = [sense for sense in vn.classids(word) if 'run' in sense]
+       print (final)
+       final = str(final).strip('['']')
+       print (final)
+       unnatural = (vn.pprint_themroles('run-51.3.2'))
+       #print ((vn.pprint_themroles(final)))
 print (lambdaexpression,final)
-
-if final == verbsenses:
-    sense = "metaphor"
-    print (sense) ##It should print out metaphor but doesn't.
